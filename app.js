1424db3fcd5af34a8265286daa714dcb99bb7a1a
spieldy <spieldennerjeremy@gmail.com>
2020-09-30 14:14:10 -0400

    request and data validation on ninjify

---
+++ app.js
@@ -0,0 +1,37 @@
+const Joi = require('joi');
+const express = require('express');
+const app = express();
+app.use(express.json());
+
+// Home 
+app.get('/', (req, res) => {
+    res.send('Ninjify home');
+});
+
+// Ninjify route
+app.get('/ninjify', (req, res) => {
+    if (!req.query.buzz) return res.status(400).send('At least one buzzword is required');
+    
+    // Array of the different buzzwords
+    const buzz = { words: req.query.buzz.split(',') }
+
+    // Input validation
+    const result = validateBuzz(buzz);
+    if (result.error) return res.status(400).send(result.error.details[0].message);
+
+    res.send(result);
+});
+
+function validateBuzz(buzz) {
+    const schema = Joi.object({
+        words: Joi.array().items(Joi.string().alphanum())
+    });
+
+    return schema.validate(buzz);
+}
+
+
+
+// Use environement variable or assign one
+const port = process.env.PORT ||  3000;
+app.listen(port, () => console.log(`Listening on ${port}...`));
