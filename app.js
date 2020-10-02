1ee613da8d165d47ae093233de6fc09bd4553a73
spieldy <spieldennerjeremy@gmail.com>
2020-10-02 11:42:25 -0400

    Revert "connection on database"
    This reverts commit 79fb7b294ecfc1f61bc69589f6046a1deaa17e0d.

--- app.js
+++ app.js
@@ -1,8 +1,10 @@
+const Joi = require("joi");
+
 const express = require("express");
 
-const ninja = require("./controllers/ninja");
-const randommer = require("./controllers/randommer");
-const data = require("./models/database");
+const ninja = require("./controller/ninja");
+const randommer = require("./controller/randommer");
+const data = require("./model/database");
 
 const app = express();
 app.use(express.json());
@@ -11,8 +13,6 @@ app.set("view engine", "ejs");
 
 // Create the first pool of requested name on the server
 const init = randommer.initSurnames();
-
-data.checkBuzz("php");
 
 // ROUTES
 // Home
--- models/database.js
+++ models/database.js
@@ -1,24 +1,7 @@
-const config = require("../config");
-const mongo = require("mongodb").MongoClient;
-
-mongo.connect(
-  config.dbUrl,
-  { useNewUrlParser: true, useUnifiedTopology: true },
-  (err, client) => {
-    if (err) return console.log(err);
-
-    // Storing a reference to the database so you can use it later
-    database = client.db(config.dbName);
-    console.log(`Connected MongoDB: ${config.dbUrl}`);
-    console.log(`Database: ${config.dbName}`);
-  }
-);
-
 var database = [];
 
 function checkBuzz(word) {
   const result = database.find((d) => d.buzz === word);
-  console.log(result);
   return result;
 }
 
